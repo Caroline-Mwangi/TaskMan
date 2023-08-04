d7e9f96802d81137100400b8b19b32d601e0ce60
CarolineMwangi <cwmwangi50@gmail.com>
2023-08-04 17:40:13 +0300

    saving registration info

--- task_man/views.py
+++ task_man/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from .models import User
 
 def home(request):
     return render(request, "task_man/index.html")
 
 def signup(request):
+    if request.method == "POST":
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        email = request.POST['email']
+        password = request.POST['pass']
+        cpassword = request.POST['cpass']
+        
+        user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+        user.save()
+        
     return render(request, "task_man/signup.html")
 
 def verify(request):
