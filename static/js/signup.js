b87848ab9af55b9ff3d350986b47a6708673f817
CarolineMwangi <cwmwangi50@gmail.com>
2023-08-16 15:31:59 +0300

    add password requirements

--- static/js/signup.js
+++ static/js/signup.js
@@ -22,14 +22,41 @@ function checkPassStrength(){
 
   const strength = zxcvbn(password);
 
-  indicator.innerHTML = `Password Strength: ${strength.score}/4`;
+  const passRequirements = 
+    password.length >= 8 &&
+    /[a-z]/.test(password) &&
+    /[A-Z]/.test(password) &&
+    /[\d]/.test(password) &&
+    /[!@#\$%\^&\*\(\)_\+{}\[\]:;<>,.?~\-]/.test(password);
 
-  if (strength.score === 4) {
+  let message = "";
+
+  if (passRequirements && strength.score === 4) {
+    indicator.innerHTML = `Password Strength: ${strength.score}/4`;
     indicator.style.color = "green";
-  } else if (strength.score >= 2) { 
-    indicator.style.color = "orange";
   } else {
-    indicator.style.color = "red";
-  }
+    if (password.length < 8) {
+      message += "<br> - Password must be at least 8 characters long";
+    }
+    if (!/[a-z]/.test(password)) {
+      message += "<br> - Include at least one lowercase letter";
+    }
+    if (!/[A-z]/.test(password)) {
+      message += "<br> - Include at least one uppercase letter";
+    } 
+    if (!/[\d]/.test(password)) {
+      message += "<br> - Include at least one number";
+    } 
+    if (!/[!@#\$%\^&\*\(\)_\+{}\[\]:;<>,.?~\-]/.test(password)) {
+      message += "<br> - Include at least one special character";
+    }
 
+    indicator.innerHTML = `Password Strength: ${strength.score}/4  ${message}`;
+    if (strength.score >= 2) {
+      indicator.style.color = "orange";
+    } else {
+      indicator.style.color="red";
+    }
+
+  }  
 }
