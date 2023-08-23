fa361a3b805516a7c45abd7d4b29365dcf8c1aa2
CarolineMwangi <cwmwangi50@gmail.com>
2023-08-23 15:13:09 +0300

    landing page modifications

--- static/js/landing.js
+++ static/js/landing.js
@@ -2,8 +2,19 @@ function dropFunc() {
     $("#dropdown").toggleClass("show");
 }
 
-$document.on("click", function(event) {
-    if(!$(event.target).closest('.drop-btn').length) {
+$(document).on("click", function(event) {
+    if(!$(event.target).closest('.drop-btn').length && !$(event.target).closest('.drop-content').length) {
         $(".drop-content").removeClass("show");
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const tabs = document.querySelectorAll(".tab");
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            tabs.forEach(t => t.classList.remove("active"));
+            tab.classList.add("active");
+        })
+    })
+})
