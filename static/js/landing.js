7b741c8eaa8ef8b75052c7feba0d06ac2adf09b2
CarolineMwangi <cwmwangi50@gmail.com>
2023-08-24 16:07:08 +0300

    feat: create tasks overlay

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,3 +1,8 @@
+const createTaskBtn = document.getElementById("createTaskBtn");
+const overlay = document.getElementById("overlay");
+const overlayBackground = document.getElementById("overlayBackground");
+const saveTaskButton = document.getElementById("saveTaskButton");
+
 function dropFunc() {
     $("#dropdown").toggleClass("show");
 }
@@ -17,4 +22,20 @@ document.addEventListener("DOMContentLoaded", function() {
             tab.classList.add("active");
         })
     })
-})
+});
+
+createTaskBtn.addEventListener("click", () => {
+    overlay.style.display = "block";
+    overlayBackground.style.display = "block";
+});
+
+saveTaskButton.addEventListener("click", () => {
+    // Handle saving the task here
+    overlay.style.display = "none";
+    overlayBackground.style.display = "none";
+});
+
+overlayBackground.addEventListener("click", () => {
+    overlay.style.display = "none";
+    overlayBackground.style.display = "none";
+});
